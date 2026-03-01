ed46950ef473d91484e68caeeaf71e21d182830a
KG48 <karangavhane1213@gmail.com>
2026-03-02 00:27:06 +0530

    Add routes

---
+++ src/app/routes.tsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from "react-router";
+import { DashboardLayout } from "./components/layout/DashboardLayout";
+import { DashboardPage } from "./pages/Dashboard";
+import { StudyTrackerPage } from "./pages/StudyTracker";
+import { CodingTrackerPage } from "./pages/CodingTracker";
+import { GoalsPage } from "./pages/Goals";
+import { DailyPlannerPage } from "./pages/DailyPlanner";
+import { AnalyticsPage } from "./pages/Analytics";
+import { ProfilePage } from "./pages/Profile";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DashboardLayout />,
+    children: [
+      { index: true, element: <DashboardPage /> },
+      { path: "study-tracker", element: <StudyTrackerPage /> },
+      { path: "coding-tracker", element: <CodingTrackerPage /> },
+      { path: "goals", element: <GoalsPage /> },
+      { path: "daily-planner", element: <DailyPlannerPage /> },
+      { path: "analytics", element: <AnalyticsPage /> },
+      { path: "profile", element: <ProfilePage /> },
+    ],
+  },
+]);
